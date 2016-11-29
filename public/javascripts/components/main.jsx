e78e604418dedd4e498196a91667447e0bf9fbfb
James Ahlm <14782828428@qq.com>
2016-11-29 17:57:00 +0800

    Changed Fonts

--- public/javascripts/components/main.jsx
+++ public/javascripts/components/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import JQuery from "jquery";
 
 export default class Main extends React.Component {
   constructor() {
@@ -10,11 +11,16 @@ export default class Main extends React.Component {
   render() {
     var showAbout = function(){
 	console.log('g');
+	JQuery(".background").animate({
+opacity: .5
+}, 1000);
+	JQuery(".about").show("slow");
+ 
 }
     return (
       <div className="main">
 	<div className="left-vertical-text"><span onClick={showAbout}>{(this.about) ?　"close" : "about"}</span></div>
-      <div className="right-vertical-text">1478282482@qq.com</div>
+      <div className="right-vertical-text"><span onClick={function(){window.location.href='mailto:1478282482@qq.com';}}>1478282482@qq.com</span></div>
 	<div className="name"　onClick={function(){console.log('d')}}>Joshua Edwards</div>
 	<div className="post">DIGITAL DESIGNER</div>
       </div>
